49fba475784c4c31afa7935829f6afdde0bfc83a
Martin Tudor <martcan27@gmail.com>
2020-12-27 14:19:16 +0000

    Created container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
 module.exports = {
     module: {
         rules: [
@@ -12,6 +15,11 @@ module.exports = {
                     }
                 }
             }
-        ]
-    }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ]
 }
---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const {merge} = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonFig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const domain = process.env.PRODUCTION_DOMNAIN;
+
+const prodConfig = {
+    mode: "production",
+    output: {
+        filename: "[name].[contenthash].js",
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "marketing",
+            filename: "remoteEntry.js",
+            exposes: {
+                "./MarketingApp": "./src/bootstrap.js"
+            },                
+            shared: packageJson.dependencies,
+        })
+    ],
+    
+};
+
+module.exports = merge(commonFig, prodConfig);
