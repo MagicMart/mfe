d57e1bfa03cc67902e19331392f255ae56dc935c
Martin Tudor <martcan27@gmail.com>
2021-01-21 16:08:11 +0000

    feature: dashboard

---
+++ packages/dashboard/config/webpack.dev.js
@@ -0,0 +1,36 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonFig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const devConfig = {
+  mode: "development",
+  output: {
+    publicPath: "http://localhost:8083/",
+  },
+  devServer: {
+    port: 8083,
+    historyApiFallback: {
+      index: "index.html",
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "dashboard",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./DashboardApp": "./src/bootstrap.js",
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(commonFig, devConfig);
